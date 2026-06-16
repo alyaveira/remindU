f3d0b6b43336b711fb6f2eab37d54adfcb0f9ca5
alyaveira <alyaveira@gmail.com>
2026-06-16 09:31:32 +0700

    update server.js and vercel.json for deployment

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production', // true di Vercel
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: true,
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
     }
 }));
